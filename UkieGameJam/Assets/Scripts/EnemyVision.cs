d66e46a000ce957b3cf8a153c6f934375603b845
andrew0milne <32519506+andrew0milne@users.noreply.github.com>
2018-10-22 18:30:50 +0100

    added enemy

---
+++ UkieGameJam/Assets/Scripts/EnemyVision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVision : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public Transform infront;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+    
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Agent")
+        {
+            RaycastHit hit;
+
+            Vector3 dir = col.transform.position - enemy.GetComponent<EnemyController>().enemy.transform.position;
+
+            if (Physics.Raycast(enemy.GetComponent<EnemyController>().enemy.transform.position, dir * 10.0f, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.tag == "Agent")
+                {
+                    enemy.GetComponent<EnemyController>().TargetSpotted(col.gameObject);
+                }
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
