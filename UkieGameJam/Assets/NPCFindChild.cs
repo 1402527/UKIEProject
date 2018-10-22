69cb6eb4439ae4924d8f82de0385e7754cfd01f7
suryahmrai <suryahmrai@gmail.com>
2018-10-22 17:29:38 +0100

    added NPC conversion

---
+++ UkieGameJam/Assets/NPCFindChild.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFindChild : MonoBehaviour {
+
+    public List<GameObject> totalNPCs;
+
+    // Use this for initializatio
+
+    void Start()
+    {
+
+        Transform[] npcChildren = GetComponentsInChildren<Transform>();
+
+        foreach (Transform g in npcChildren)
+        {
+            if (g.tag == "NPC")
+            {
+                totalNPCs.Add(g.gameObject);
+            }
+
+
+        }
+    }
+}
