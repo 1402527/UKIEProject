3049a61dc9ee763093f4ff2278e49fc10cd9b8c2
suryahmrai <suryahmrai@gmail.com>
2018-10-22 23:33:49 +0100

    Revert "Merge branch 'master' of https://github.com/1402527/UKIEProject"
    This reverts commit bce2ceaec6327107761c17b81f8f195a6a842a29, reversing
    changes made to 84e8cb1a33805dee9b8b2a6e4c25a935f66106fe.

--- UkieGameJam/Assets/Scripts/SecretEnemyController.cs
+++ UkieGameJam/Assets/Scripts/SecretEnemyController.cs
@@ -28,8 +28,6 @@ public class SecretEnemyController : MonoBehaviour
     public float turn_speed = 5.0f;
     public float speed;
 
-    public Light spotlight;
-
     // Use this for initialization
     void Start()
     {
@@ -60,9 +58,6 @@ public class SecretEnemyController : MonoBehaviour
         agent.speed = speed;
 
         agent.destination = patrol_positions[current_position];
-
-        spotlight = enemy_parent.GetComponent<Light>();
-       
     }
 
     IEnumerator LookAround()
@@ -124,7 +119,7 @@ public class SecretEnemyController : MonoBehaviour
     public void TargetSpotted(GameObject target_loc)
     {
         Debug.Log("Enemy Spotted");
-        StartCoroutine(EnemyReveal());
+
         //anim.enabled = false;
 
         target = target_loc.transform;
@@ -161,13 +156,5 @@ public class SecretEnemyController : MonoBehaviour
 
         }
 
-    }
-
-    IEnumerator EnemyReveal()
-    {
-        print(Time.time);
-        spotlight.enabled = true;
-        yield return new WaitForSeconds(2);
-        print(Time.time);
     }
 }
