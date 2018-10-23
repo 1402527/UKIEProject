609e93735d780daf2cbbbd42383cb9db7391509c
suryahmrai <suryahmrai@gmail.com>
2018-10-23 16:08:22 +0100

    main menu

---
+++ UkieGameJam/Assets/AudioStatus.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AudioStatus : MonoBehaviour {
+
+    public AudioSource gameAudio;
+    private int numberofAudioSources;
+
+
+    private void Awake()
+    {
+        numberofAudioSources = FindObjectsOfType<AudioStatus>().Length;
+
+        if(numberofAudioSources>1)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        
+
+	}
+}
