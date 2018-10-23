0fadd73eb3ad618de3605a584d9f01eed4460486
andrew0milne <32519506+andrew0milne@users.noreply.github.com>
2018-10-23 13:30:37 +0100

    menu shit

---
+++ UkieGameJam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Image credits;
+    bool credits_b = false;
+
+	public void StartButton()
+    {
+        //SceneManager.LoadScene("level_main");
+        Debug.Log("Clisck");
+    }
+
+    public void Credits()
+    {
+        credits_b = !credits_b;
+        credits.enabled = credits_b;
+    }
+}
